f657b904630435802313b4f698b3b8e64eaac21c
M rivaldo <151760350+MRivaldo14@users.noreply.github.com>
2025-05-19 13:52:15 +0700

    Delete routes directory

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-import HomeView from '../views/homeView.js';
-import HomePresenter from '../presenters/homePresenter.js';
-
-export default async function home() {
-  HomeView.init();
-
-  const presenter = HomePresenter(HomeView);
-  await presenter.loadStories(); 
-}
